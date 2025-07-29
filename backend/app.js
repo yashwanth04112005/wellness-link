9ab68c59ed7b714c45e9889f3618f50c1cef4298
yashwanth04112005 <paladugulayashwanth2005@gmail.com>
2025-07-29 22:42:15 +0530

    final change path _to_regex_error

--- backend/app.js
+++ backend/app.js
@@ -6,7 +6,7 @@ import authRoutes from './routes/authRoutes.js';
 import sessionRoutes from './routes/sessionRoutes.js';
 import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 import path from 'path';
-import { fileURLToPath } from 'url'; 
+import { fileURLToPath } from 'url';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -17,8 +17,8 @@ connectDB();
 const app = express();
 app.use(cors());
 
-app.use(express.json()); 
-app.use(express.urlencoded({ extended: true })); 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.use('/api/auth', authRoutes);
 app.use('/api', sessionRoutes);
@@ -26,9 +26,13 @@ app.use('/api', sessionRoutes);
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.resolve(__dirname, '../frontend/dist')));
 
-  app.get('*', (req, res) =>
-    res.sendFile(path.resolve(__dirname, '../frontend/dist', 'index.html'))
-  );
+  app.use((req, res, next) => {
+    if (req.method === 'GET' && !req.path.startsWith('/api')) {
+      res.sendFile(path.resolve(__dirname, '../frontend/dist/index.html'));
+    } else {
+      next();
+    }
+  });
 } else {
   app.get('/', (req, res) => {
     res.send('API is running in development mode...');
